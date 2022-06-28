9df1fac6777fc5f37105e024463bd5485bcc8044
Viktoria <Mo.Viks@yandex.ru>
2022-06-28 14:01:27 +0300

    Seminar 5+

--- Seminar_5/Program.cs
+++ Seminar_5/Program.cs
@@ -48,47 +48,6 @@
 // [-4, -8, 8, 2] -> [4, 8, -8, -2]
 
 
-// int[] CreateRandomArray(int size, int min, int max){
-//     int[] array = new int[size];
-//     for (int i = 0; i < size; i++)
-//     {
-//         array[i] = new Random().Next(min, max + 1);
-//     }
-//     return array;
-// }
-
-// void ShowArray(int[] array){
-//     for (int i = 0; i < array.Length; i++)
-//     {
-//         Console.Write(array[i] + "  ");
-//     }
-//     Console.WriteLine();
-// }
-
-// Console.WriteLine("Введите количество элементов: ");
-// int S = Convert.ToInt32(Console.ReadLine());
-// Console.WriteLine("Введите минимальный элемент: ");
-// int m = Convert.ToInt32(Console.ReadLine());
-// Console.WriteLine("Введите максимальный элемент: ");
-// int M = Convert.ToInt32(Console.ReadLine());
-// int[] newArray = CreateRandomArray(S, m, M);
-
-// ShowArray(newArray);
-
-// for (int i = 0; i < S; i++)
-// {
-//     newArray[i] *= -1; 
-// }
-
-// ShowArray(newArray);
-
-
-
-
-// 3. Найдите произведение пар чисел в одномерном массиве. Парой считаем первый и последний элемент, второй и предпоследний и т.д. Результат запишите в новом массиве.
-// [1 2 3 4 5] -> 5 8 3
-// [6 7 3 6] -> 36 21
-
 int[] CreateRandomArray(int size, int min, int max){
     int[] array = new int[size];
     for (int i = 0; i < size; i++)
@@ -106,25 +65,66 @@ void ShowArray(int[] array){
     Console.WriteLine();
 }
 
-void ProductMassiv(int[] array){
-    int result = 1;
-    int length = array.Length / 2;
-   for (int i = 0; i <= length; i++)
-   {
-    result = array[i] * array[array.Length - 1 - i];
-    Console.WriteLine(result);
-   } 
-}
-
 Console.WriteLine("Введите количество элементов: ");
 int S = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите минимальный элемент: ");
 int m = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите максимальный элемент: ");
 int M = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine();
-
 int[] newArray = CreateRandomArray(S, m, M);
 
 ShowArray(newArray);
-ProductMassiv(newArray);
+
+for (int i = 0; i < S; i++)
+{
+    newArray[i] *= -1; 
+}
+
+ShowArray(newArray);
+
+
+
+
+// 3. Найдите произведение пар чисел в одномерном массиве. Парой считаем первый и последний элемент, второй и предпоследний и т.д. Результат запишите в новом массиве.
+// [1 2 3 4 5] -> 5 8 3
+// [6 7 3 6] -> 36 21
+
+// int[] CreateRandomArray(int size, int min, int max){
+//     int[] array = new int[size];
+//     for (int i = 0; i < size; i++)
+//     {
+//         array[i] = new Random().Next(min, max + 1);
+//     }
+//     return array;
+// }
+
+// void ShowArray(int[] array){
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         Console.Write(array[i] + "  ");
+//     }
+//     Console.WriteLine();
+// }
+
+// void ProductMassiv(int[] array){
+//     int result = 1;
+//     int length = array.Length / 2;
+//    for (int i = 0; i <= length; i++)
+//    {
+//     result = array[i] * array[array.Length - 1 - i];
+//     Console.WriteLine(result);
+//    } 
+// }
+
+// Console.WriteLine("Введите количество элементов: ");
+// int S = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите минимальный элемент: ");
+// int m = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите максимальный элемент: ");
+// int M = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine();
+
+// int[] newArray = CreateRandomArray(S, m, M);
+
+// ShowArray(newArray);
+// ProductMassiv(newArray);
