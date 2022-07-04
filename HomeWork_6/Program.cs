7952202d94cff8d4952c28a29ed3fac25c6b71b0
Viktoria <Mo.Viks@yandex.ru>
2022-07-04 11:04:32 +0300

    HomeWork 6

--- HomeWork_6/Program.cs
+++ HomeWork_6/Program.cs
@@ -86,3 +86,12 @@
 // Если N = 3 -> 0 1 1
 // Если N = 7 -> 0 1 1 2 3 5 8
 
+int[] arr = new int[5];
+arr[0] = 0;
+arr[1] = 1;
+// int i = 2;
+for (int i = 2; i < arr.Length; i++)
+{
+    arr[i] = arr[i - 1] + arr[i - 2];
+    Console.Write($"{arr[i]}");
+} 
